32c4cf42dfe29e747a77cc4984252b17269ab20c
Koded <sogoseriki@gmail.com>
2023-02-04 13:15:14 +0100

    .

---
+++ Array.js
@@ -0,0 +1,23 @@
+//////////////////////////////////JAVASCRIPT PARSE INT EXERCISE 1//////////////////////
+function randomFraction() {
+
+    // Only change code below this line
+    return Math.floor( Math.random() *20)
+    // Only change code above this line
+  }
+  console.log(randomFraction())
+  console.log(randomFraction())
+  console.log(randomFraction())
+  function convertToInteger(str) {
+    return parseInt(str);
+  }
+  console.log(convertToInteger("565"))
+  a = convertToInteger("8")
+  b=a*a
+  b
+  
+  let a = 99999
+  let b = 192990
+  
+  console.log(a > b ? "a is grater" : "b is greater")
+  
