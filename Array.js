04f72ffcdf3f70f0427d8b2e2787bd784e1ca00b
Koded <sogoseriki@gmail.com>
2023-02-05 18:04:23 +0100

    .

--- Array.js
+++ Array.js
@@ -12,12 +12,57 @@ function randomFraction() {
     return parseInt(str);
   }
   console.log(convertToInteger("565"))
-  a = convertToInteger("8")
-  b=a*a
-  b
   
   let a = 99999
   let b = 192990
   
   console.log(a > b ? "a is grater" : "b is greater")
   
+/////////////////////////////////////JAVASCRIPT SPLICE EXERCISE///////////////////////
+const arr = [2, 4, 5, 1, 7, 5, 2, 1];
+// Only change code below this line
+arr.splice(1,4)
+// Only change code above this line
+console.log(arr);
+
+/////////////////////////////////////JAVASCRIPT SPLICE EXERCISE 2///////////////////////
+function htmlColorNames(arr) {
+  // Only change code below this line
+arr.splice(0,1, 'DarkSalmon', 'BlanchedAlmond')
+  // Only change code above this line
+  return arr;
+}
+
+console.log(htmlColorNames(['DarkGoldenRod', 'WhiteSmoke', 'LavenderBlush', 'PaleTurquoise', 'FireBrick']));
+/////////////////////////////////////JAVASCRIPT SLICE EXERCISE 1///////////////////////
+function forecast(arr) {
+  // Only change code below this line
+  return arr.slice(2,4)
+  
+}
+
+// Only change code above this line
+console.log(forecast(['cold', 'rainy', 'warm', 'sunny', 'cool', 'thunderstorms']));
+
+/////////////////////////////////////JAVASCRIPT SPREAD EXERCISE 1///////////////////////
+function copyMachine(arr, num) {
+  let newArr = [];
+  while (num >= 1) {
+  newArr.push([...arr])
+    num--;
+  }
+  return newArr;
+}
+
+console.log(copyMachine([true, false, true], 2));  
+
+
+/////////////////////////////////////JAVASCRIPT SPREAD EXERCISE 2///////////////////////
+function spreadOut() {
+  let fragment = ['to', 'code'];
+  let sentence= ['learning',...fragment,"is", "fun"] 
+  return sentence;
+}
+
+console.log(spreadOut());
+
